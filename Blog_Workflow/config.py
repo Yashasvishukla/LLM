e6970e5b60e1ced5a21d700b3345a80c655a9d63
Yashasvishukla <35268031+Yashasvishukla@users.noreply.github.com>
2025-06-06 10:25:18 +0530

    Initial Commit

---
+++ Blog_Workflow/config.py
@@ -0,0 +1,13 @@
+import os
+from enum import Enum
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TYPEFULLY_API_URL = "https://api.typefully.com/v1/drafts/"
+TYPEFULLY_API_KEY = os.getenv("TYPEFULLY_API_KEY")
+HEADERS = {"X-API-KEY": f"Bearer {TYPEFULLY_API_KEY}"}
+
+class PostType(Enum):
+    TWITTER = "Twitter"
+    LINKEDIN = "LinkedIn"
